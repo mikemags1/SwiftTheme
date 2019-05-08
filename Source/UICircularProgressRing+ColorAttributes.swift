35397418bedf92ab75797b6d5d37de6f9ee2afe5
Mike Maguire <mike@bluechxp.com>
2019-05-08 16:14:43 -0500

    added UICircularProgressRing dependency

---
+++ Source/UICircularProgressRing+ColorAttributes.swift
@@ -0,0 +1,24 @@
+////
+////  UICircularProgressRing.swift
+////  SwiftTheme
+////
+////  Created by Bluechxp on 5/8/19.
+////  Copyright © 2019 Gesen. All rights reserved.
+////
+//
+//import Foundation
+//import UICircularProgressRing
+//
+//extension UICircularProgressRing {
+//    @objc func setInnerRingColor(_ color: UIColor) {
+//        self.innerRingColor = color
+//    }
+//    
+//    @objc func setOuterRingColor(_ color: UIColor) {
+//        self.outerRingColor = color
+//    }
+//    
+//    @objc func setFontColor(_ color: UIColor) {
+//        self.fontColor = color
+//    }
+//}
--- Source/UIKit+Theme.swift
+++ Source/UIKit+Theme.swift
@@ -9,11 +9,21 @@
 import UIKit
 import UICircularProgressRing
 
-@objc public extension UIView
+@objc public extension UICircularProgressRing
 {
     var theme_innerRingColor: ThemeColorPicker? {
         get { return getThemePicker(self, "setInnerRingColor:") as? ThemeColorPicker }
         set { setThemePicker(self, "setInnerRingColor:", newValue) }
+    }
+    
+    var theme_outerRingColor: ThemeColorPicker? {
+        get { return getThemePicker(self, "setOuterRingColor:") as? ThemeColorPicker }
+        set { setThemePicker(self, "setOuterRingColor:", newValue) }
+    }
+    
+    var theme_fontColor: ThemeColorPicker? {
+        get { return getThemePicker(self, "setFontColor:") as? ThemeColorPicker }
+        set { setThemePicker(self, "setFontColor:", newValue) }
     }
 }
 @objc public extension UIView
