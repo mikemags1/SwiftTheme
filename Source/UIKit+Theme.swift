d409a6d29b9f0d36c43c8a15de50c3a344cfb351
Mike Maguire <mike@bluechxp.com>
2019-05-08 16:50:37 -0500

    added UICircularProgressRing dependency

--- Source/UIKit+Theme.swift
+++ Source/UIKit+Theme.swift
@@ -6,7 +6,25 @@
 //  Copyright © 2016年 Gesen. All rights reserved.
 //
 import UIKit
+import UICircularProgressRing
 
+@objc public extension UICircularProgressRing
+{
+    var theme_innerRingColor: ThemeColorPicker? {
+        get { return getThemePicker(self, "setInnerRingColor:") as? ThemeColorPicker }
+        set { setThemePicker(self, "setInnerRingColor:", newValue) }
+    }
+    
+    var theme_outerRingColor: ThemeColorPicker? {
+        get { return getThemePicker(self, "setOuterRingColor:") as? ThemeColorPicker }
+        set { setThemePicker(self, "setOuterRingColor:", newValue) }
+    }
+    
+    var theme_fontColor: ThemeColorPicker? {
+        get { return getThemePicker(self, "setFontColor:") as? ThemeColorPicker }
+        set { setThemePicker(self, "setFontColor:", newValue) }
+    }
+}
 @objc public extension UIView
 {
     var theme_alpha: ThemeCGFloatPicker? {
